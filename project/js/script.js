74eb1f721d267e71f1a296b8e1c15c010679e3d7
Nevin Katz <nevkatz@gmail.com>
2022-05-30 22:26:34 -0400

    moved room-specific functions into new room_helpers.js file

--- project/js/script.js
+++ project/js/script.js
@@ -215,144 +215,6 @@ function startGame() {
         console.log('enemies: ' + game.enemies.length);
     }
 }
-
-function labelRooms() {
-    game.context.fillStyle = 'black';
-    game.context.font = '15px Arial';
-    game.rooms.forEach(function(room) {
-
-        let txt = `r${room.id} (${room.start.x},${room.start.y})`;
-
-        game.context.fillText(txt, (room.start.x + 1) * TILE_DIM, room.center.y * TILE_DIM);
-    });
-}
-
-
-
-
-/**
- * Randomly generates a set of dimensions.
- * 
- */
-function genDim(baseDim, added, roomType) {
-    const BASE_DIM = baseDim || 6;
-
-    let width, height;
-
-    width = height = BASE_DIM;
-
-    const EXTRA_RANGE = 5;
-
-    let additional = added || Math.round(Math.random() * EXTRA_RANGE);
-
-    if (!roomType) {
-        roomType = Math.random() < 0.5 ? 'tall' : 'wide';
-    }
-
-    if (roomType == 'tall') {
-        height += additional;
-    } else {
-        width += additional;
-    }
-    return {
-        width,
-        height
-    };
-}
-
-/**
- * 
- * @param {Object} center
- * @param {Number} height
- * @param {Number} width
- * 
- */
-function setRoomCoords(center, width, height) {
-
-
-    let halfW = Math.round(width / 2);
-    let halfH = Math.round(height / 2);
-
-    let start = {
-        x: center.x - halfW,
-        y: center.y - halfH
-    };
-
-    let end = {
-        x: center.x + halfW,
-        y: center.y + halfH
-    };
-
-    return {
-        start,
-        end
-    };
-}
-/**
- * Generates one room based on a center point.
- * @param {Object} center {x,y}
- */
-function generateRoom(center, width, height) {
-
-    // get coordinates based on width and height
-    let {
-        start,
-        end
-    } = setRoomCoords(center, width, height);
-
-    let room = new Room(center, start, end);
-
-    room.id = game.curRoomId;
-
-    return room;
-
-}
-
-function addRoom(coords, baseDim, additional, roomType) {
-
-    let { width,height} = genDim(baseDim, additional, roomType);
-
-    const genCenterCoord = (maxCells, dim) => {
-        // get limit on either side based on outer limit and a room dimension - width or height
-        let limit = OUTER_LIMIT + Math.round(dim / 2);
-
-        // get range based on cells in array - limit on either side.
-        let range = maxCells - 2 * limit;
-
-        // get a random  number within 
-        return limit + Math.round(Math.random() * range);
-    }
-
-    coords = coords || {
-        x: genCenterCoord(COLS, width),
-        y: genCenterCoord(ROWS, height)
-    }
-
-    let room = generateRoom(coords, width, height);
-
-    for (var gameRoom of game.rooms) {
-
-        if (room.overlaps(gameRoom, 1)) {
-            return null;
-        }
-
-    }
-
-    game.curRoomId++;
-    game.roomToMap(room);
-    game.rooms.push(room);
-    return room;
-
-}
-
-
-
-function printNeighbors() {
-    for (var room of game.rooms) {
-        let ids = room.neighbors.map(x => x.id);
-
-    }
-}
 /**
  * @param {Number} quantity - the number of items to generate
  * @param {Number} tileCode - corresponds to a constant, such as POTION_CODE.
@@ -493,20 +355,6 @@ function pickRandom(arr) {
     return arr[idx];
 }
 
-function generatePlayer() {
-
-    // let coords = generateValidCoords();
-
-    let coords = {
-        x: COLS / 2,
-        y: ROWS / 2
-    };
-
-    // level, health, weapon, coords, xp
-    player = new Player(1, 100, WEAPONS[0], coords, 30, 0);
-
-    addObjToMap(player.coords, PLAYER_CODE);
-}
 
 // add given coords to map 
 // make the coords and neighbors busy
@@ -573,6 +421,20 @@ function generateShadow() {
             }
         }
     }
+}
+function generatePlayer() {
+
+    // let coords = generateValidCoords();
+
+    let coords = {
+        x: COLS / 2,
+        y: ROWS / 2
+    };
+
+    // level, health, weapon, coords, xp
+    player = new Player(1, 100, WEAPONS[0], coords, 30, 0);
+
+    addObjToMap(player.coords, PLAYER_CODE);
 }
 
 /**
